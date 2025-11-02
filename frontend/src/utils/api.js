09eb4e1885e5c5270a6ef9b2d923ce7bb2e3c348
Rohan Kumar <146082711+rohankumar62@users.noreply.github.com>
2025-11-02 16:38:47 +0530

    Update API base URL to production endpoint

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://linkedinclone-gmtc.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
